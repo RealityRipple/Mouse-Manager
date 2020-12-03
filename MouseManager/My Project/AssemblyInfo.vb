828a7729fdb60e42f36addd7c49e785e0b6ee041
Andy <webmaster@RealityRipple.com>
2020-12-03 13:59:26 -0800

    Increment Version Numbers

--- MouseManager/My Project/AssemblyInfo.vb
+++ MouseManager/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("9135592C-4B41-5177-9650-46CE32B74205")> 
+<Assembly: Guid("9135592C-4B41-5177-9650-46CE32B74205")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.6")> 
-<Assembly: AssemblyFileVersion("1.6")> 
+<Assembly: AssemblyVersion("2.0")>
+<Assembly: AssemblyFileVersion("2.0")>
